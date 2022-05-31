c9e71cacacf8900bb0bdf1185712f4da4df84643
zhaokanghua <zhaokanghua@mokahr.com>
2022-05-31 20:33:10 +0800

    ✨

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { useState, useCallback, useMemo } from 'react'
+// Import the Slate editor factory.
+import { createEditor, Transforms, Editor, Text, Node } from 'slate'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+import CustomElement from './CustomElement'
+import DefaultElement from './DefaultElement'
+
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code'
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  }
 }
 
-export default App;
+// Define a serializing function that takes a value and returns a string.
+const serialize = value => {
+  return (
+    value
+      // Return the string content of each paragraph in the value's children.
+      .map(n => Node.string(n))
+      // Join them all with line breaks denoting paragraphs.
+      .join('\n')
+  )
+}
+
+// Define a deserializing function that takes a string and returns a value.
+const deserialize = string => {
+  // Return a value array of children derived by splitting the string.
+  return string.split('\n').map(line => {
+    return {
+      children: [{ text: line }]
+    }
+  })
+}
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+
+  const initialValue = useMemo(
+    () => deserialize(localStorage.getItem('content')) || '',
+    []
+  )
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CustomElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  const renderLeaf = useCallback(props => {
+    return (
+      <span
+        {...props.attributes}
+        style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+      >
+        {props.children}
+      </span>
+    )
+  }, [])
+
+  return (
+    <Slate
+      editor={editor}
+      value={initialValue}
+      onChange={value => {
+        const isAstChange = editor.operations.some(
+          op => 'set_selection' !== op.type
+        )
+        if (isAstChange) {
+          // Serialize the value and save the string value to Local Storage.
+          localStorage.setItem('content', serialize(value))
+        }
+      }}
+    >
+      <div>
+        <button
+          onClick={event => {
+            event.preventDefault()
+            CustomEditor.toggleBoldMark(editor)
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onClick={event => {
+            event.preventDefault()
+            CustomEditor.toggleCodeBlock(editor)
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return
+          }
+
+          // Replace the `onKeyDown` logic with our new commands.
+          switch (event.key) {
+            case '`': {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+              break
+            }
+
+            case 'b': {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+              break
+            }
+          }
+        }}
+      />
+    </Slate>
+  )
+}
+
+export default App
